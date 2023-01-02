3fb3bf660fcc7a48773c4378cfa50beb0249451d
vishalkrsharma <vishalkrsharma1234@gmail.com>
2023-01-02 14:34:21 +0530

    MCM

---
+++ 3_Matrix_Chain_Multiplication/Scrambled_String_Memoization.java
@@ -0,0 +1,3 @@
+public class Scrambled_String_Memoization {
+
+}
---
+++ 3_Matrix_Chain_Multiplication/Scrambled_String_Recursive.java
@@ -0,0 +1,41 @@
+public class Scrambled_String_Recursive {
+
+  public static boolean mcm(String a, String b) {
+    if (a.length() != b.length()) {
+      return false;
+    }
+    if (a.length() == 0 && b.length() == 0) {
+      return false;
+    }
+
+    if (a.compareTo(b) == 0) {
+      return true;
+    }
+
+    if (a.length() <= 1) {
+      return false;
+    }
+
+    int n = a.length();
+    boolean flag = false;
+
+    for (int i = 1; i < n; i++) {
+      if (mcm(a.substring(0, i), b.substring(n - i, n)) && mcm(a.substring(i, n - i), b.substring(0, n - i))) {
+        flag = true;
+        break;
+      }
+      if (mcm(a.substring(0, i), b.substring(0, i)) && mcm(a.substring(i, n - i), b.substring(i, n - i))) {
+        flag = true;
+        break;
+      }
+    }
+    return flag;
+  }
+
+  public static void main(String[] args) {
+    String a = "we";
+    String b = "we";
+
+    System.out.println(mcm(a, b));
+  }
+}
